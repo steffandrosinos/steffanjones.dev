27726bf8cd0d062a780d9123ecb0b714967ea1b9
Steffan Jones <SteffanDrosinos@outlook.com>
2019-11-18 00:24:00 +0000

    minor changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Personal website made in html, css and served in flask
+# Personal website made in javascript, jQuery, html, css and flask
+
+This is the code for my personal website. The items not in this repository are my wsgi files and setup files. Right now I only have the cli page working so I redirect all traffic to cli.steffanjones.dev for now.
--- flask_app.py
+++ flask_app.py
@@ -2,20 +2,22 @@ from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
-#Redirect 404
+# Redirect 404
 @app.errorhandler(404)
 def handle_404(e):
     return render_template("404.html")
 
-#Redirect non-www traffic
+# non-www traffic
 @app.route('/')
 def non_www_index():
     return redirect("https://cli.steffanjones.dev", code=302)
 
+# www traffic
 @app.route('/', subdomain="www")
 def index():
     return redirect("https://cli.steffanjones.dev", code=302)
-    
+
+# cli traffic
 @app.route('/', subdomain="cli")
 def cli():
     return render_template("index.html")
