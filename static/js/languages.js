04b241e79491670900325d50354a4cf71624d944
Steffan Jones <SteffanDrosinos@outlook.com>
2019-11-29 15:01:35 +0000

    minor tweaks

---
+++ static/js/languages.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+
+  //Java
+  $("#logo_java").hover(function() {
+    $("#lang_java").css("visibility", "visible");
+  }, function(){
+    $("#lang_java").css("visibility", "hidden");
+  });
+
+  //Python
+  $("#logo_python").hover(function() {
+    $("#lang_python").css("visibility", "visible");
+  }, function(){
+    $("#lang_python").css("visibility", "hidden");
+  });
+
+  //JS
+  $("#logo_js").hover(function() {
+    $("#lang_js").css("visibility", "visible");
+  }, function(){
+    $("#lang_js").css("visibility", "hidden");
+  });
+
+  //html
+  $("#logo_html").hover(function() {
+    $("#lang_html").css("visibility", "visible");
+  }, function(){
+    $("#lang_html").css("visibility", "hidden");
+  });
+
+  //css
+  $("#logo_css").hover(function() {
+    $("#lang_css").css("visibility", "visible");
+  }, function(){
+    $("#lang_css").css("visibility", "hidden");
+  });
+
+  //C
+  $("#logo_c").hover(function() {
+    $("#lang_c").css("visibility", "visible");
+  }, function(){
+    $("#lang_c").css("visibility", "hidden");
+  });
+
+  //C++
+  $("#logo_cpp").hover(function() {
+    $("#lang_cpp").css("visibility", "visible");
+  }, function(){
+    $("#lang_cpp").css("visibility", "hidden");
+  });
+
+  //Haskell
+  $("#logo_haskell").hover(function() {
+    $("#lang_haskell").css("visibility", "visible");
+  }, function(){
+    $("#lang_haskell").css("visibility", "hidden");
+  });
+
+});
